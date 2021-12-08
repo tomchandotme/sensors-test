e89a97f2625dad51175d715b5d7a52ec9394b69e
tomchan <tomchan@instelar.com>
2021-12-08 16:07:53 +0800

    gyro #14

--- src/App.svelte
+++ src/App.svelte
@@ -33,9 +33,9 @@
 
         accelerometer.i = e.interval;
 
-        orientation.x += e.rotationRate.beta * e.interval;
-        orientation.y += e.rotationRate.gamma * e.interval;
-        orientation.z += e.rotationRate.alpha * e.interval;
+        orientation.x += e.rotationRate.alpha * e.interval;
+        orientation.y += e.rotationRate.beta * e.interval;
+        orientation.z += e.rotationRate.gamma * e.interval;
     };
 
     const start = (e: MouseEvent) => {
